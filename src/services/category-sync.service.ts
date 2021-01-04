0d6b878e3ad5f871dcdff19368ccb8c0f48a8540
Bruno Costa <bruno.costa@superlogica.com>
2021-01-04 15:09:59 -0300

    espacos

--- src/services/category-sync.service.ts
+++ src/services/category-sync.service.ts
@@ -7,24 +7,24 @@ import {ValidatorService} from "./validator.service";
 
 @bind({scope: BindingScope.SINGLETON})
 export class CategorySyncService extends BaseSyncService{
-  constructor(
-      @repository(CategoryRepository) private repo: CategoryRepository,
-      @service(ValidatorService) private validator: ValidatorService
-  ) {
-      super(validator)
-  }
+    constructor(
+        @repository(CategoryRepository) private repo: CategoryRepository,
+        @service(ValidatorService) private validator: ValidatorService
+    ) {
+        super(validator)
+    }
 
-  @RabbitmqSubscribe({
-      exchange: 'amq.topic',
-      queue: 'ms-catalogo/sync-videos/category',
-      routingKey: 'model.category.*'
-  })
-  async handler({data, action}: {data: any, action: any}) {
-      await this.sync({
-          repo: this.repo,
-          data,
-          action
-      })
+    @RabbitmqSubscribe({
+        exchange: 'amq.topic',
+        queue: 'ms-catalogo/sync-videos/category',
+        routingKey: 'model.category.*'
+    })
+    async handler({data, action}: {data: any, action: any}) {
+        await this.sync({
+            repo: this.repo,
+            data,
+            action
+        })
 
-  }
+    }
 }
